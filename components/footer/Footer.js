e1ffac17cb15b3a1528030a8f3463add5f5e9d04
haw0k <cyril.zlachevsky@gmail.com>
2020-10-07 13:31:23 +0300

    html lang added; lighthouse recomendations applied

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -4,33 +4,42 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4 col-md-3">
-            <div className="footer__copyright">
+    <footer className='footer'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-4 col-md-3'>
+            <div className='footer__copyright'>
               <p>© Gloria Logistic, 2020</p>
               <p>
-                {t("common:footerCreated")}{" "}
-                <a href="https://t.me/haw0k" target="_blank">
-                  {t("common:footerCreator")}
-                </a>
+                <small>
+                  {t("common:footerCreated")}{" "}
+                  <a
+                    href='https://t.me/haw0k'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {t("common:footerCreator")}
+                  </a>
+                </small>
               </p>
             </div>
           </div>
-          <div className="col-lg-5 col-md-6">
-            <div className="footer__address">
-              <p>{t('common:footerAddress')}:</p>
+          <div className='col-lg-5 col-md-6'>
+            <div className='footer__address'>
+              <p>{t("common:footerAddress")}:</p>
               <address>
                 Paseo Mediterráneo, n 1, local 10, despacho 6, 03590 - Altea
                 (Alicante)
               </address>
             </div>
           </div>
-          <div className="col-md-3 d-flex justify-content-md-end">
-            <div className="footer__email">
+          <div className='col-md-3 d-flex justify-content-md-end'>
+            <div className='footer__email'>
               <p>e-mail:</p>
-              <a href="mailto: glorialogistic.s.l@gmail.com">
+              <a
+                href='mailto: glorialogistic.s.l@gmail.com'
+                rel='noopener noreferrer'
+              >
                 glorialogistic.s.l@gmail.com
               </a>
             </div>
